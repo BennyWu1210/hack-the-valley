972322c2419e9fcaec18994e27febea2e492ec33
bennywu2019 <benny.wu.new@gmail.com>
2024-10-05 13:56:49 -0400

    shadcn configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
-export default function Home() {
 
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
   return (
-    <div>Home</div>
+    <Button>Home</Button>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}', // If using the app router
   ],
   theme: {
-    extend: {},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
